28bc403fdc3b7e078c302e31ed62fb5ee393c34b
pranjalg1331 <pranjalgwalior@gmail.com>
2023-09-07 09:52:24 +0530

    new models

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from register.models import Profile
 # Create your models here.
 class Category(models.Model):
     id=models.AutoField
@@ -23,4 +23,16 @@ class Product(models.Model):
         return Product.objects.filter(category=id)
 
     
+class Cart(models.Model):
+    id=models.AutoField
+    user=models.OneToOneField(Profile,on_delete=models.CASCADE)
+    total_price=models.DecimalField(max_digits=10,decimal_places=2)
+    
+    
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.SmallIntegerField()
+    
+
     
